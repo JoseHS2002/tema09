6c3638dcdcfef7b96e526dcf6a45538b12687fda
JoseHS2002 <joherso16@gmail.com>
2025-04-07 17:35:57 +0200

    Ejercicio5678

--- src/main/java/com/josehs/tema09/Ejercicio7/divisas.java
+++ src/main/java/com/josehs/tema09/Ejercicio7/divisas.java
@@ -8,26 +8,26 @@ public class divisas {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Map<String, Double> tasasDeCambio = new HashMap<>();
+        Map<String, Double> cambioEuro = new HashMap<>();
 
-        tasasDeCambio.put("USD", 1.10);
-        tasasDeCambio.put("GBP", 0.85);
-        tasasDeCambio.put("INR", 94.10);
-        tasasDeCambio.put("AUD", 1.81);
-        tasasDeCambio.put("CAD", 1.56);
-        tasasDeCambio.put("ARS", 1177.69);
-        tasasDeCambio.put("BOB", 7.62);
-        tasasDeCambio.put("CLP", 1047.51);
-        tasasDeCambio.put("VEZ", 4584.20);
-        tasasDeCambio.put("CRC", 558.00);
-        tasasDeCambio.put("CUP", 26.47);
-        tasasDeCambio.put("DOP", 69.66);
-        tasasDeCambio.put("MXN", 22.66);
+        cambioEuro.put("USD", 1.10);
+        cambioEuro.put("GBP", 0.85);
+        cambioEuro.put("INR", 94.10);
+        cambioEuro.put("AUD", 1.81);
+        cambioEuro.put("CAD", 1.56);
+        cambioEuro.put("ARS", 1177.69);
+        cambioEuro.put("BOB", 7.62);
+        cambioEuro.put("CLP", 1047.51);
+        cambioEuro.put("VEZ", 4584.20);
+        cambioEuro.put("CRC", 558.00);
+        cambioEuro.put("CUP", 26.47);
+        cambioEuro.put("DOP", 69.66);
+        cambioEuro.put("MXN", 22.66);
 
-        System.out.println("Dime cuantos euros");
+        System.out.println("Dime la cantidad de euros:");
         double euros = scanner.nextDouble();
 
-        for (Map.Entry<String, Double> entrada : tasasDeCambio.entrySet()) {
+        for (Map.Entry<String, Double> entrada : cambioEuro.entrySet()) {
             String moneda = entrada.getKey();
             double tasa = entrada.getValue();
             double cantidadConvertida = euros * tasa;
